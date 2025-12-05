c857875ecc78b56aa060b48bcf4a5630e0a8ff33
RikuTwT <12674@edu.wheatlandhigh.org>
2025-12-05 22:52:32 +0000

    YEs

--- PI.py
+++ PI.py
@@ -1,3 +1,7 @@
+x = int(input("number: "))
+
+
+
 try:
     # import version included with old SymPy
     from sympy.mpmath import mp
@@ -5,5 +9,5 @@ except ImportError:
     # import newer version
     from mpmath import mp
 
-mp.dps = 1000000 # set number of digits
+mp.dps = x # set number of digits
 print(mp.pi)   # print pi to a thousand places
---
+++ Work.py
@@ -0,0 +1,25 @@
+# James Nickell
+
+# Period 7
+
+# File I/O practice
+
+# 2:23 
+
+file = open("Myfile.txt",'w')
+content = str(input("Write a word: "))
+content2 = str(input("Another word: "))
+content3 = str(input("Third one: "))
+file.write(content+'\n')
+file.write(content2+'\n')
+file.write(content3+'\n')
+file.close
+
+file = open('Myfile.txt','r')
+file_line_1 = (file.readline().rstrip('\n'))
+file_line_2 = (file.readline().rstrip('\n'))
+file_line_3 = (file.readline().rstrip('\n'))
+print(file_line_1)
+print(file_line_2)
+print(file_line_3) 
+file.close
