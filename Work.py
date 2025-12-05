d498c5a4e7f4b7d2306aabd9bb39da37c2271a05
RikuTwT <12674@edu.wheatlandhigh.org>
2025-12-05 22:55:29 +0000

    Yes

--- Work.py
+++ Work.py
@@ -5,7 +5,7 @@
 # File I/O practice
 
 # 2:23 - 2:51
-
+# Opens a new file, then asks you to provide the contents of said file.
 file = open("Myfile.txt",'w')
 content = str(input("Write a word: "))
 content2 = str(input("Another word: "))
@@ -15,6 +15,7 @@ file.write(content2+'\n')
 file.write(content3+'\n')
 file.close
 
+# Opens the file again to allow it to be printed and given to the user
 file = open('Myfile.txt','r')
 file_line_1 = (file.readline().rstrip('\n'))
 file_line_2 = (file.readline().rstrip('\n'))
